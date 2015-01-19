14dda6861c2493628b14d73b9872673fd5a93abc
Remy Bach <remy@bach.me.uk>
2015-01-19 08:55:30 +0000

    Remove ActionView::Helpers

--- lib/requirejs/rails/view_proxy.rb
+++ lib/requirejs/rails/view_proxy.rb
@@ -2,8 +2,8 @@ module Requirejs
   module Rails
     class ViewProxy
       include ActionView::Context
-      include ActionView::Helpers::AssetUrlHelper
-      include ActionView::Helpers::TagHelper
+      # include ActionView::Helpers::AssetUrlHelper
+      # include ActionView::Helpers::TagHelper
     end
   end
 end
